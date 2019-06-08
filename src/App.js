a41e7d8391eaaf7b655f6b3ab7a41faa754b4349
Aditya Tibrewal <aditya7bgp@gmail.com>
2019-06-08 15:12:38 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { ZAPI } from "./constants";
+import { debounce } from "lodash-es";
 
 function App() {
+  const [collections, setCollections] = useState([]);
+  const fetchCollections = () => {
+    console.log("fecthing");
+    fetch(ZAPI)
+      .then(res => res.json())
+      .then(({ response }) => {
+        console.log(response.collections);
+        setCollections(response.collections);
+        window.collections = response.collections;
+      });
+  };
+  useEffect(() => {
+    fetchCollections();
+  }, []);
+
+  const filterCollections = event => {
+    const ntarget = event.target;
+    console.log(event.target.value);
+    const val = ntarget.value;
+
+    const newCollections = collections.filter(
+      item => item.title.indexOf(val) !== -1
+    );
+    if (!val) {
+      setCollections(window.collections);
+    } else {
+      setCollections(newCollections);
+    }
+  };
+
+  const handler = event => {
+    event.persist();
+    debouncedSearch(event);
+  };
+
+  const debouncedSearch = debounce(filterCollections, 200);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Zomato Collections
+      <div>
+        <input type="text" onChange={handler} />
+        <ul>
+          {collections.length
+            ? collections.map((item, index) => (
+                <li key={index}>{item.title}</li>
+              ))
+            : null}
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const API =
+  "https://www.zomato.com/webapi/handlers/Collections/index.php?type=collections_featured&city_id=1&offset=0&limit=30&zpwa=true";
+
+export const ZAPI = "http://localhost:8080/";
